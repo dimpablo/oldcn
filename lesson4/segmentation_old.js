c8ba1bb6328288f3edd9903765c89e3962c7135e
dimpablo <dimpablo@yandex.ru>
2025-08-30 17:49:42 +0300

    auto: update 2025-08-30 17:49

---
+++ lesson4/segmentation_old.js
@@ -0,0 +1,60 @@
+const SentenceData = {
+  "lesson": 4,
+  "text": "壬寅卜㱿貞自今至于甲辰子商烖基\n方自今至于甲辰子商弗其烖基方自\n今壬寅至于甲辰子商烖基方自今至\n于丁巳我烖賽王占曰丁巳我毋其烖\n于來甲子烖\n甲子卜中貞求年自上甲求自上甲癸\n亥卜中貞求年自上甲至于多后貞于\n示壬求年\n其自西來雨其自東來雨其自南來雨\n其自北來雨來雨自西自今五日不雨",
+  "sentences": [
+    {
+      "number": 1,
+      "original": "壬寅卜㱿貞自今至于甲辰子商烖基方",
+      "transliteration": "rén yín bǔ jué zhēn zì jīn zhì yú jiǎ chén zǐ shāng zāi jī fāng",
+      "translation": "В день жэнь-инь гадатель Цзюэ задал вопрос: «С сегодняшнего дня и до цзя-чзнь сын по имени Шан нанесет поражение племени Цзи-фан?»."
+    },
+    {
+      "number": 2,
+      "original": "方自今至于甲辰子商弗其烖基方",
+      "transliteration": "fāng zì jīn zhì yú jiǎ chén zǐ shāng fú qí zāi jī fāng",
+      "translation": "Сегодняшнего дня и до цзя-чэнь сын по имени Шан не нанесет поражения племени Цзи-фан?"
+    },
+    {
+      "number": 3,
+      "original": "方自今壬寅至于甲辰子商烖基方",
+      "transliteration": "fāng zì jīn rén yín zhì yú jiǎ chén zǐ shāng zāi jī fāng",
+      "translation": "С нынешнего дня жэнь-инь и до цзя-чэнь сын по имени Шан нанесет поражение племени Цзи-фан?"
+    },
+    {
+      "number": 4,
+      "original": "方自今至于丁巳我烖賽",
+      "transliteration": "fāng zì jīn zhì yú dīng sì wǒ zāi sài",
+      "translation": "С сегодняшнего дня и до дин-сы мы нанесем поражение Сай?"
+    },
+    {
+      "number": 5,
+      "original": "王占曰丁巳我毋其烖于來甲子烖",
+      "transliteration": "wáng zhān yuē dīng sì wǒ wú qí zāi yú lái jiǎ zǐ zāi",
+      "translation": "Ван прочел ответ: «В день дин-сы нам не следует наносить поражение. Нанесем поражение в следующий день цзя-цзы»."
+    },
+    {
+      "number": 6,
+      "original": "甲子卜中貞求年自上甲求自上甲",
+      "transliteration": "jiǎ zǐ bǔ zhōng zhēn qiú nián zì shàng jiǎ qiú zì shàng jiǎ",
+      "translation": "В день цзя-цзы гадатель Чжун задал вопрос: «Следует ли просить урожай у Верхнего Цзя? Следует ли просить у Верхнего Цзя?»."
+    },
+    {
+      "number": 7,
+      "original": "癸亥卜中貞求年自上甲至于多后",
+      "transliteration": "guǐ hài bǔ zhōng zhēn qiú nián zì shàng jiǎ zhì yú duō hòu",
+      "translation": "В день гуй-хай гадатель Чжун задал вопрос: «Следует ли просить урожай начиная с Верхнего Цзя до многих покойных правителей?»."
+    },
+    {
+      "number": 8,
+      "original": "貞于示壬求年",
+      "transliteration": "zhēn yú shì rén qiú nián",
+      "translation": "Вопрос: «У предка Жэнь просить урожай?»."
+    },
+    {
+      "number": 9,
+      "original": "其自西來雨其自東來雨其自南來雨其自北來雨來雨自西自今五日不雨",
+      "transliteration": "qí zì xī lái yǔ qí zì dōng lái yǔ qí zì nán lái yǔ qí zì běi lái yǔ lái yǔ zì xī zì jīn wǔ rì bù yǔ",
+      "translation": "«С запада придет дождь? С востока придет дождь? С юга придет дождь? С севера придет дождь? Придет ли дождь с запада? Начиная с сегодняшнего дня в течение пяти дней не будет дождя?»."
+    }
+  ]
+};
---
+++ lesson5/segmentation_old.js
@@ -0,0 +1,66 @@
+const SentenceData = {
+  "lesson": 5,
+  "text": "辛卯卜㱿貞帝其熯我\n貞帝不我熯戊申卜爭貞帝其降我熯\n貞帝不我降熯甲辰卜爭貞我伐馬方帝受我佑\n貞勿伐工方帝不我其受佑貞洹其作茲邑禍\n洹弗作茲邑禍\n貞帝其作王禍余受馬方佑乙亥卜㱿貞王曰侯虎\n余其得汝使余不其合以乃使\n甲辰卜王貞羌弗烖朕使其烖余不余烖",
+  "sentences": [
+    {
+      "number": 1,
+      "original": "辛卯卜㱿貞帝其熯我",
+      "transliteration": "xīn mǎo bǔ jué zhēn dì qí hàn wǒ",
+      "translation": "В день синь-мао гадатель Цзюэ задал вопрос: «Верховное божество накажет нас засухой?»"
+    },
+    {
+      "number": 2,
+      "original": "貞帝不我熯",
+      "transliteration": "zhēn dì bù wǒ hàn",
+      "translation": "Вопрос: «Верховное божество не накажет нас засухой?»"
+    },
+    {
+      "number": 3,
+      "original": "戊申卜爭貞帝其降我熯",
+      "transliteration": "wù shēn bǔ zhēng zhēn dì qí jiàng wǒ hàn",
+      "translation": "В день у-шэнь гадатель Чжэн задал вопрос: «Верховное божество нашлет на нас засуху?»"
+    },
+    {
+      "number": 4,
+      "original": "貞帝不我降熯",
+      "transliteration": "zhēn dì bù wǒ jiàng hàn",
+      "translation": "Вопрос: «Верховное божество не нашлет на нас засуху?»"
+    },
+    {
+      "number": 5,
+      "original": "甲辰卜爭貞我伐馬方帝受我佑",
+      "transliteration": "jiǎ chén bǔ zhēng zhēn wǒ fá mǎ fāng dì shòu wǒ yòu",
+      "translation": "В день цзя-чэнь гадатель Чжэн задал вопрос: «Мы пойдем походом на племя Ма-фан? Ниспошлет ли нам Верховное божество свою помощь?»"
+    },
+    {
+      "number": 6,
+      "original": "貞勿伐工方帝不我其受佑",
+      "transliteration": "zhēn wù fá gōng fāng dì bù wǒ qí shòu yòu",
+      "translation": "Вопрос: «Нам не следует идти походом на племя Гун-фан? Верховное божество не ниспошлет нам помощи?»"
+    },
+    {
+      "number": 7,
+      "original": "貞洹其作茲邑禍洹弗作茲邑禍",
+      "transliteration": "zhēn huán qí zuò zī yì huò huán fú zuò zī yì huò",
+      "translation": "Вопрос: «Река Хуань причинит вред этому городу? Река Хуань не причинит вреда этому городу?»"
+    },
+    {
+      "number": 8,
+      "original": "貞帝其作王禍余受馬方佑",
+      "transliteration": "zhēn dì qí zuò wáng huò yú shòu mǎ fāng yòu",
+      "translation": "Вопрос: «Верховное божество причинит вред вану? Я окажу помощь племени Ма-фан?»"
+    },
+    {
+      "number": 9,
+      "original": "乙亥卜㱿貞王曰侯虎余其得汝使余不其合以乃使",
+      "transliteration": "yǐ hài bǔ jué zhēn wáng yuē hóu hǔ yú qí dé rǔ shǐ yú bù qí hé yǐ nǎi shǐ",
+      "translation": "В день и-хай гадатель Цзюэ задал вопрос: «Скажет ли ван: „Хоу Ху, я сохраняю тебе посла. Я не встречусь с тобой. Приведи твоего посла!“?»"
+    },
+    {
+      "number": 10,
+      "original": "甲辰卜王貞羌弗烖朕使其烖余不余烖",
+      "transliteration": "jiǎ chén bǔ wáng zhēn qiāng fú zāi zhèn shǐ qí zāi yú bù yú zāi",
+      "translation": "В день цзя-чэнь гадали, правитель задал вопрос: «Цяны не нанесут поражения моему послу? Он подведет меня? Не подведет меня?»"
+    }
+  ]
+};
