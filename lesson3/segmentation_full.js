eaf48572a6c381e10faf86fe4a2e44d4d0bb968b
dimpablo <dimpablo@yandex.ru>
2025-09-02 18:05:45 +0300

    auto: update 2025-09-02 18:05

---
+++ lesson3/segmentation_full.js
@@ -0,0 +1,132 @@
+const SentenceData = {
+  "lesson": 3,
+  "text": "庚寅卜㱿貞來乙巳王勿入于商\n貞來辛丑王入于商王其入大邑商\n今六月王入于商王于生七月入于商\n九月我入商\n貞勿求于妣庚貞今日求于大庚于大甲\n求求大甲求妣辛\n貞翌庚寅王告王于甲午告勿告于中丁\n勿于大甲告勿于大戊告貞告工方\n于上甲于大甲告于大丁告工方\n貞于河告貞于唐告",
+  "sentences": [
+    {
+      "number": 1,
+      "original": "庚寅卜㱿貞來乙巳王勿入于商",
+      "transliteration": "gēng yín bǔ jué zhēn lái yǐ sì wáng wù rù yú shāng",
+      "translation": "В день гэн-инь гадатель Цзюэ задал вопрос: «В следующий день и-сы правителю не следует ли вступить в Шан?»."
+    },
+    {
+      "number": 2,
+      "original": "貞來辛丑王入于商",
+      "transliteration": "zhēn lái xīn chǒu wáng rù yú shāng",
+      "translation": "Вопрос: «В следующий день синь-чоу правитель вступит в Шан?»"
+    },
+    {
+      "number": 3,
+      "original": "王其入大邑商",
+      "transliteration": "wáng qí rù dà yì shāng",
+      "translation": "Правитель вступит в Великий город Шан?"
+    },
+    {
+      "number": 4,
+      "original": "今六月王入于商",
+      "transliteration": "jīn liù yuè wáng rù yú shāng",
+      "translation": "В нынешнем шестом месяце правитель вступит в Шан?"
+    },
+    {
+      "number": 5,
+      "original": "王于生七月入于商",
+      "transliteration": "wáng yú shēng qī yuè rù yú shāng",
+      "translation": "В начинающемся седьмом месяце правитель вступит в Шан?"
+    },
+    {
+      "number": 6,
+      "original": "九月我入商",
+      "transliteration": "jiù yuè wǒ rù shāng",
+      "translation": "В девятом месяце мы вступим в Шан?"
+    },
+    {
+      "number": 7,
+      "original": "貞勿求于妣庚",
+      "transliteration": "zhēn wù qiú yú bǐ gēng",
+      "translation": "Вопрос: «Не следует просить у прародительницы Гэн?»"
+    },
+    {
+      "number": 8,
+      "original": "貞今日求于大庚",
+      "transliteration": "zhēn jīn rì qiú yú dà gēng",
+      "translation": "Вопрос: «Сегодня будем просить у Великого Гэна?»"
+    },
+    {
+      "number": 9,
+      "original": "于大甲求",
+      "transliteration": "yú dà jiǎ qiú",
+      "translation": "Будем просить у Великого Цзя?"
+    },
+    {
+      "number": 10,
+      "original": "求大甲",
+      "transliteration": "qiú dà jiǎ",
+      "translation": "Просить у Великого Цзя?"
+    },
+    {
+      "number": 11,
+      "original": "求妣辛",
+      "transliteration": "qiú bǐ xīn",
+      "translation": "Просить у прародительницы Синь?"
+    },
+    {
+      "number": 12,
+      "original": "貞翌庚寅王告",
+      "transliteration": "zhēn yì gēng yín wáng gào",
+      "translation": "Вопрос: «В следующий день гэн-инь правитель сообщит?»"
+    },
+    {
+      "number": 13,
+      "original": "王于甲午告",
+      "transliteration": "wáng yú jiǎ wǔ gào",
+      "translation": "Правитель сообщит в день цзя-у?"
+    },
+    {
+      "number": 14,
+      "original": "勿告于中丁",
+      "transliteration": "wù gào yú zhōng dīng",
+      "translation": "Не следует сообщать Среднему Дину?"
+    },
+    {
+      "number": 15,
+      "original": "勿于大甲告",
+      "transliteration": "wù yú dà jiǎ gào",
+      "translation": "Не следует сообщать Великому Цзя?"
+    },
+    {
+      "number": 16,
+      "original": "勿于大戊告",
+      "transliteration": "wù yú dà wù gào",
+      "translation": "Не следует сообщать Великому У?"
+    },
+    {
+      "number": 17,
+      "original": "貞告工方于上甲",
+      "transliteration": "zhēn gào gōng fāng yú shàng jiǎ",
+      "translation": "Вопрос: «Сообщить Верхнему Цзя о племени Гун-фан?»"
+    },
+    {
+      "number": 18,
+      "original": "于大甲告",
+      "transliteration": "yú dà jiǎ gào",
+      "translation": "Сообщить Великому Цзя?"
+    },
+    {
+      "number": 19,
+      "original": "于大丁告工方",
+      "transliteration": "yú dà dīng gào gōng fāng",
+      "translation": "Сообщить о племени Гун-фан Великому Дину?"
+    },
+    {
+      "number": 20,
+      "original": "貞于河告",
+      "transliteration": "zhēn yú hé gào",
+      "translation": "Вопрос: «Сообщить реке?»"
+    },
+    {
+      "number": 21,
+      "original": "貞于唐告",
+      "transliteration": "zhēn yú táng gào",
+      "translation": "Вопрос: «Сообщить Тану?»"
+    }
+  ]
+}
