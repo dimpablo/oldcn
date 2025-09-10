121c1fc7935b7e7181d5f9c0cedb692e74758921
dimpablo <dimpablo@yandex.ru>
2025-09-10 15:58:01 +0300

    auto: update 2025-09-10 15:58

--- singleline.sh
+++ singleline.sh
@@ -1,1 +1,3 @@
-for d in lesson{1..21}; do [[ -d "$d" ]] && cp translateall.html "$d/"; done
+#for d in lesson{1..60}; do [[ -d "$d" ]] && cp translateall.html "$d/"; done
+for d in lesson{1..60}; do [[ -d "$d" ]] && cp vocab.html "$d/"; done
+for d in lesson{1..60}; do [[ -d "$d" ]] && cp hanzi.html "$d/"; done
